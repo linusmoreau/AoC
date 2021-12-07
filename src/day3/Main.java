50b10c32bbb62055a2ccc5ab0e86acb40f9b59da
Linus Moreau <linus.moreau@gmail.com>
2021-12-07 00:10:49 -0800

    Rework implementation of Day 3

--- src/day3/Main.java
+++ src/day3/Main.java
@@ -15,98 +15,87 @@ public class Main {
         return total;
     }
 
-    private static int task1(ArrayList<String> lines) {
-        int length = lines.get(0).length();
-        int[] totals = new int[length];
+    private static int toDecimal(ArrayList<Integer> binary) {
+        int total = 0;
+        for (int i = 0; i < binary.size(); i++) {
+            total += binary.get(binary.size() - i - 1) * Math.pow(2, i);
+        }
+        return total;
+    }
+
+    private static int mostCommon(int i, ArrayList<String> lines) {
+        int total0 = 0;
+        int total1 = 0;
         for (String line : lines) {
-            for (int i = 0; i < length; i++) {
-                if (line.charAt(i) == '0') {
-                    totals[i] -= 1;
-                } else if (line.charAt(i) == '1') {
-                    totals[i] += 1;
-                } else {
-                    throw new RuntimeException("Not allowable character");
-                }
+            if (line.charAt(i) == '0') {
+                total0 += 1;
+            } else if (line.charAt(i) == '1') {
+                total1 += 1;
+            } else {
+                throw new RuntimeException("Not allowable character");
             }
         }
+        if (total0 > total1) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    private static int task1(ArrayList<String> lines) {
+        int length = lines.get(0).length();
         int[] gamma = new int[length];
         int[] epsilon = new int[length];
         for (int i = 0; i < length; i++) {
-            if (totals[i] > 0) {
-                gamma[i] = 1;
+            int mostCommon = mostCommon(i, lines);
+            gamma[i] = mostCommon;
+            if (mostCommon == 1) {
                 epsilon[i] = 0;
-            } else if (totals[i] < 0) {
-                gamma[i] = 0;
-                epsilon[i] = 1;
             } else {
-                throw new RuntimeException("You're bad");
+                epsilon[i] = 1;
             }
         }
         return toDecimal(gamma) * toDecimal(epsilon);
     }
 
-    private static int[] filterOxygen(ArrayList<String> lines, int index, char[] oxygen) {
-        int total = 0;
-        for (String line : lines) {
-            if (line.charAt(index) == '0') {
-                total -= 1;
-            } else if (line.charAt(index) == '1') {
-                total += 1;
-            } else {
-                throw new RuntimeException("Not allowable character");
-            }
-        }
-        if (total >= 0) {
-            oxygen[index] = 1;
-        } else {
-            oxygen[index] = 0;
-        }
-        lines.removeIf(s -> (Integer.parseInt(String.valueOf(s.charAt(index))) != oxygen[index]));
+    private static ArrayList<Integer> filterOxygen(int i, ArrayList<String> lines) {
+        int mostCommon = mostCommon(i, lines);
+        lines.removeIf(s -> (Integer.parseInt(String.valueOf(s.charAt(i))) != mostCommon));
         if (lines.size() == 1) {
             char[] line = lines.get(0).toCharArray();
-            int[] out = new int[line.length];
-            for (int i = 0; i < line.length; i++) {
-                out[i] = Integer.parseInt(String.valueOf(line[i]));
+            ArrayList<Integer> out = new ArrayList<>();
+            for (char c : line) {
+                out.add(Integer.parseInt(String.valueOf(c)));
             }
             return out;
         } else {
-            return filterOxygen(lines, index + 1, oxygen);
+            return filterOxygen(i + 1, lines);
         }
     }
 
-    private static int[] filterCO2(ArrayList<String> lines, int index, char[] oxygen) {
-        int total = 0;
-        for (String line : lines) {
-            if (line.charAt(index) == '0') {
-                total -= 1;
-            } else if (line.charAt(index) == '1') {
-                total += 1;
-            } else {
-                throw new RuntimeException("Not allowable character");
-            }
-        }
-        if (total >= 0) {
-            oxygen[index] = 0;
+    private static ArrayList<Integer> filterCO2(int i, ArrayList<String> lines) {
+        int leastCommon;
+        if (mostCommon(i, lines) == 1) {
+            leastCommon = 0;
         } else {
-            oxygen[index] = 1;
+            leastCommon = 1;
         }
-        lines.removeIf(s -> (Integer.parseInt(String.valueOf(s.charAt(index))) != oxygen[index]));
+        lines.removeIf(s -> (Integer.parseInt(String.valueOf(s.charAt(i))) != leastCommon));
         if (lines.size() == 1) {
             char[] line = lines.get(0).toCharArray();
-            int[] out = new int[line.length];
-            for (int i = 0; i < line.length; i++) {
-                out[i] = Integer.parseInt(String.valueOf(line[i]));
+            ArrayList<Integer> out = new ArrayList<>();
+            for (char c : line) {
+                out.add(Integer.parseInt(String.valueOf(c)));
             }
             return out;
         } else {
-            return filterCO2(lines, index + 1, oxygen);
+            return filterCO2(i + 1, lines);
         }
     }
 
     private static int task2(ArrayList<String> lines) {
-        int length = lines.get(0).length();
-        return toDecimal(filterOxygen((ArrayList<String>) lines.clone(), 0, new char[length]))
-                * toDecimal(filterCO2((ArrayList<String>) lines.clone(), 0, new char[length]));
+        return toDecimal(filterOxygen(0, (ArrayList<String>) lines.clone()))
+                * toDecimal(filterCO2(0, (ArrayList<String>) lines.clone()));
     }
 
     public static void main(String[] args) {
